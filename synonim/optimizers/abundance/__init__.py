2f40db19f423915004e9360bc0e09a2328541391
Benjamin Luke Coltman <benjamin.coltman@univie.ac.at>
2025-09-30 14:01:20 +0200

    Removing abundance based

--- synonim/optimizers/abundance/__init__.py
+++
@@ -1,3 +0,0 @@
-# from synonim.optimizers.abundance.abundance_heuristic import *
-from synonim.optimizers.abundance.abundance_genetic import *
-from synonim.optimizers.abundance.abundance_milp import *
